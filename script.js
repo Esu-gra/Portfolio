50ee32d6fe4f54cbbec78d0105fb2d0563629138
esu grappasonni <esugrappa@gmail.com>
2024-02-08 00:07:51 +0100

    create navbar end header

---
+++ script.js
@@ -0,0 +1,8 @@
+const typed = new Typed(".text", {
+    strings: ["Junior Full Stack developer","Back-end developer"], 
+    typeSpeed: 100,
+    backSpeed: 20,
+    backDelay: 1000,
+    loop:true
+    
+});
