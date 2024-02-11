995e195730cb2c06169a146ed83263a9bbbd2e3d
esu grappasonni <esugrappa@gmail.com>
2024-02-11 22:14:28 +0100

    finish navbar and  body implematetion

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const typed = new Typed(".text", {
-    strings: ["Junior Full Stack developer","Back-end developer"], 
+    strings: ["Back-end developer"], 
     typeSpeed: 100,
     backSpeed: 20,
     backDelay: 1000,
