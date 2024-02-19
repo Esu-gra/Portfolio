907c4562cd1badc6f9ebc247b6b9c005b2d2cfbc
esu grappasonni <esugrappa@gmail.com>
2024-02-19 19:22:27 +0100

    modifica front-end

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const typed = new Typed(".text", {
-    strings: ["Back-end developer"], 
+    strings: ["Junior developer"], 
     typeSpeed: 100,
     backSpeed: 20,
     backDelay: 1000,
     loop:true
-    
+
 });
